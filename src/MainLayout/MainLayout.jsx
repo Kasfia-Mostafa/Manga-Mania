3119c4e8e285be437b035a1f17e0566af7eb1ff4
SwornaCode <kasfiamostafa03@gmail.com>
2023-12-26 22:32:13 +0600

    toggle dark mode added

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../Pages/HomePage/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="px-10">
       <Navbar></Navbar>
     <Outlet></Outlet>
     </div>
